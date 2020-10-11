8ac588ca6981f7478e89773b4deb1b19361547bb
super.kang <1689242311@qq.com>
2020-10-12 00:27:24 +0800

    2020-10-12 topK re code

--- src/middle/sort/TopKFrequentElements.java
+++ src/middle/sort/TopKFrequentElements.java
@@ -12,34 +12,33 @@ import java.util.PriorityQueue;
  */
 public class TopKFrequentElements {
     public int[] topKFrequent(int[] nums, int k) {
-        Map<Integer, Integer> occurrences = new HashMap<Integer, Integer>();
-        for (int num : nums) {
-            occurrences.put(num, occurrences.getOrDefault(num, 0) + 1);
+        Map<Integer,Integer> map=new HashMap<>();
+        PriorityQueue<int[]> pq=new PriorityQueue<>(new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[1]-o2[1];
+            }
+        }) ;
+        for(int i=0;i<nums.length;i++){
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
         }
 
-        // int[] 的第一个元素代表数组的值，第二个元素代表了该值出现的次数
-        PriorityQueue<int[]> queue = new PriorityQueue<int[]>(new Comparator<int[]>() {
-            public int compare(int[] m, int[] n) {
-                return m[1] - n[1];
-            }
-        });
-        for (Map.Entry<Integer, Integer> entry : occurrences.entrySet()) {
-            int num = entry.getKey(), count = entry.getValue();
-            if (queue.size() == k) {
-                if (queue.peek()[1] < count) {
-                    queue.poll();
-                    queue.offer(new int[]{num, count});
+        for(Map.Entry<Integer,Integer> entry:map.entrySet()){
+            int num=entry.getKey(),count=entry.getValue();
+            if(pq.size()==k){
+                if(count>pq.peek()[1]){
+                    pq.poll();
+                    pq.offer(new int[]{num,count});
                 }
-            } else {
-                queue.offer(new int[]{num, count});
+            }else{
+                pq.offer(new int[]{num,count});
             }
         }
-        int[] ret = new int[k];
-        for (int i = 0; i < k; ++i) {
-            ret[i] = queue.poll()[0];
+        int[] ret=new int[k];
+        for(int i=0;i<k;i++){
+            ret[i]=pq.poll()[0];
         }
         return ret;
-
-
     }
+
 }
